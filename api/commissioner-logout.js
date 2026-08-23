cbc5752086b3c3eb2ae4795a53b98615e364121e
kyleboucher09-crypto <kyleboucher09@gmail.com>
2026-08-23 01:17:15 -0400

    Protect commissioner logout request

--- api/commissioner-logout.js
+++ api/commissioner-logout.js
@@ -1,8 +1,9 @@
-import { json, clearCommissionerCookie } from './_common.js';
+import { json, clearCommissionerCookie, noStore, sameOrigin } from './_common.js';
 
 export default async function handler(req,res){
-  res.setHeader('Cache-Control','no-store');
+  noStore(res);
   if(req.method!=='POST') return json(res,405,{error:'Method not allowed'});
+  if(!sameOrigin(req)) return json(res,403,{error:'Forbidden'});
   clearCommissionerCookie(res);
   return json(res,200,{ok:true});
 }
