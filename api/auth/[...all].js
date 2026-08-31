98e3c4c0620a53ff12a79f8587cc9c7bb7b0f5fb
kyleboucher09-crypto <kyleboucher09@gmail.com>
2026-08-31 18:00:38 -0400

    Use shared auth schema initializer

--- api/auth/[...all].js
+++ api/auth/[...all].js
@@ -1,21 +1,5 @@
-import { getMigrations } from 'better-auth/db/migration';
 import { auth } from '../../lib/efl-auth.js';
-
-let schemaReady = globalThis.__eflBetterAuthSchemaReady || null;
-async function ensureAuthSchema() {
-  if (!schemaReady) {
-    schemaReady = (async () => {
-      const { runMigrations } = await getMigrations(auth.options);
-      await runMigrations();
-    })().catch(err => {
-      schemaReady = null;
-      globalThis.__eflBetterAuthSchemaReady = null;
-      throw err;
-    });
-    globalThis.__eflBetterAuthSchemaReady = schemaReady;
-  }
-  return schemaReady;
-}
+import { ensureAuthSchema } from '../../lib/efl-auth-schema.js';
 
 export default {
   async fetch(request) {
